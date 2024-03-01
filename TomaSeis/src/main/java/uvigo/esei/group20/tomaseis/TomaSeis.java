ac7e9386e4f2994745165b8337454ca78fe137ff
Lucas <93008975+Fenix37@users.noreply.github.com>
2024-03-01 13:58:58 +0100

    Add files via upload

---
+++ TomaSeis/src/main/java/uvigo/esei/group20/tomaseis/TomaSeis.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package uvigo.esei.group20.tomaseis;
+
+/**
+ *
+ * @author felix
+ */
+public class TomaSeis {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
