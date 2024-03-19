16b58372f02b7a5127e29becba00a97af2a05b34
IagoCernadas <iagocernadas625@gmail.com>
2024-03-19 19:34:53 +0100

    Update Juego.java

--- Proyecto_Toma6_Plantilla/src/main/java/gal/uvigo/esei/aed1/Toma6/core/Juego.java
+++ Proyecto_Toma6_Plantilla/src/main/java/gal/uvigo/esei/aed1/Toma6/core/Juego.java
@@ -7,12 +7,13 @@ package gal.uvigo.esei.aed1.Toma6.core;
 
 
 import gal.uvigo.esei.aed1.Toma6.iu.IU;
+import java.util.Stack;
 
 
 public class Juego {
-    
+    private int numXogadores;
     private final IU iu;
-
+    
     
     public Juego(IU iu){
         this.iu = iu;
@@ -23,7 +24,27 @@ public class Juego {
         
 
     }
-    
+    /**
+     * 
+     * @param baralla: Baraja con todalas cartas do xogo para dividir en grupos
+     * @return pila coas cartas do xogador
+     */
+    public Baraja barajar(Baraja baralla){
+        int random;
+        Baraja baralladas=  new Baraja();
+        //recorre baralla e (para cada carta) decide se a mete en barajadas ou non
+        while(baralladas.numCartas()<10){
+            random=((int)(Math.random()*23))%2;
+            if(random==1){
+                baralladas.addCarta(baralla.pop());
+            }   
+        }
+       return baralladas; 
+    }
+
+    public int getNumeroJugadores() {
+        return numXogadores;
+    }
     
     
 }
