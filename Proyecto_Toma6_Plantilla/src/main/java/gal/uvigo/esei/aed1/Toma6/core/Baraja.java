a130542c7e4b57db88de6873a0e26e99a9ab8cfc
IagoCernadas <iagocernadas625@gmail.com>
2024-03-20 23:40:48 +0100

    .

--- Proyecto_Toma6_Plantilla/src/main/java/gal/uvigo/esei/aed1/Toma6/core/Baraja.java
+++ Proyecto_Toma6_Plantilla/src/main/java/gal/uvigo/esei/aed1/Toma6/core/Baraja.java
@@ -1,18 +1,23 @@
-
 /*
 * Representa la baraja del juego Toma 6, en total 104 cartas, enumeradas del 1 al 104 con el número de bueyes
 * correspondiente en función del valor de la misma (revisar condiciones en el enunciado del juego). 
-* Estructura: se utilizará un TAD adecuado
-* Funcionalidad: barajar las cartas, devolver la carta situada encima del montón de cartas
+
+@@ -7,9 +6,136 @@
  */
 package gal.uvigo.esei.aed1.Toma6.core;
+
 
 import java.util.Stack;
 
 public class Baraja {
+    
+    
 
-    Stack<Carta> baraja;
+    private Stack<Carta> baraja;
 
+    /**
+     * Crea la baraja predeterminada con las 104 cartas ordenadas de mayor a menor.
+     */
     public Baraja() {
         baraja = new Stack();
         //Valor predeterminado
@@ -33,61 +38,73 @@ public class Baraja {
                     }
                 }
             }
-            baraja.add(new Carta(numBueyes, i));
+            addCarta(new Carta(numBueyes, i));
         }
     }
-
-    public Baraja(boolean esVacia) {
-        baraja = new Stack();
-    }
+    /**
+     *Crea una baraja nueva con la pila que se le pasa como parámetro.
+     * @param baraja
+     */
     public Baraja(Stack<Carta> baraja){
         this.baraja = baraja;
     }
     public void addCarta(Carta carta) {
         baraja.push(carta);
     }
-
+    /**
+     *
+     * @return Devuelve el número de cartas de la baraja.
+     */
     public int getNumCartas() {
         return baraja.size();
     }
-
+    /**
+     *
+     * @return Devuelve la última carta de la baraja sin modificarla.
+     */
     public Carta getTop() {
         return baraja.peek();
     }
-
-    //Devuelve el ultimo objeto y lo borra
+    /**
+     *
+     * @return Devuelve la última carta de la baraja y la elimina.
+     */
     public Carta getPop() {
         return baraja.pop();
     }
-
+    /**
+     * Crea una baraja vacía.
+     * @param esVacia Variable para que el constructor se diferencie del predeterminado
+     */
+    private Baraja(boolean esVacia){
+        baraja = new Stack();
+    }
+    /**
+     * 
+     * @return Devuelve una baraja vacía
+     */
+    public static Baraja crearBarajaVacía() {
+        return new Baraja(true);
+    }
+    /**
+     *
+     * @param carta: Carta a insertar en la baraja.
+     */
+    /**
+     *
+     * @return Devuelve true si la baraja está vacía y false si no
+     */
     public boolean esVacia() {
         return baraja.isEmpty();
     }
+    /**
+     *
+     * @return Devuelve la propia baraja ordenada vaciándola en el proceso.
+     */
+    public void ordenarBaraja() {
 
-    public Baraja ordenarBaraja() {
-        int num = baraja.size();
-        Stack <Carta> toret = new Stack();
-        Stack <Carta> aux = new Stack();
-        toret.add(baraja.pop());
-        while (!baraja.empty()) {
-            if(baraja.peek().getNumCarta() > toret.peek().getNumCarta()) {
-                toret.add(baraja.pop());
-            }
-            else{
-                while(toret.peek().getNumCarta() > baraja.peek().getNumCarta() || toret.empty()){
-                    aux.add(toret.pop());
-                }
-                toret.add(baraja.pop());
-                while(!aux.empty()){
-                    toret.add(aux.pop());
-                    
-                }
-            }
-        }
-
-        return new Baraja(toret);
+        
     }
-    
     /**
      *Modifica this, barajándola de forma aleatoria.
      */
@@ -114,6 +131,10 @@ public class Baraja {
         }
     }
 
+    /**
+     *
+     * @return Devuelve todas las cartas de la baraja como carta1 \n carta2 \n carta3 etc.
+     */
     @Override
     public String toString() {
         int tam = baraja.size();
