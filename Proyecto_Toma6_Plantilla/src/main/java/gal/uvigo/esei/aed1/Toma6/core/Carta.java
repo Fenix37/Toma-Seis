352ce2ba082aef088ccd2490e90280ffa57952ca
NoeLaLia <noelila1908@gmail.com>
2024-03-20 15:50:37 +0100

    Actualizacion clase carta y baraja
    Pues eso

--- Proyecto_Toma6_Plantilla/src/main/java/gal/uvigo/esei/aed1/Toma6/core/Carta.java
+++ Proyecto_Toma6_Plantilla/src/main/java/gal/uvigo/esei/aed1/Toma6/core/Carta.java
@@ -1,7 +1,6 @@
-
 /*
- * Representa una carta, formada por un número y la cantidad de bueyes correspondiente
- */
+ 
+Representa una carta, formada por un número y la cantidad de bueyes correspondiente*/
 package gal.uvigo.esei.aed1.Toma6.core;
 
 
@@ -10,16 +9,35 @@ public class Carta {
     private int numBueyes;
     private int numCarta;
 
+    /**
+     *
+     * @param numBueyes Número de bueyes de la carta.
+     * @param numCarta  Número de la carta-
+     */
     public Carta(int numBueyes, int numCarta) {
         this.numBueyes = numBueyes;
         this.numCarta = numCarta;
     }
+    /**
+     *
+     * @return Devuelve el número de bueyes que tiene asignados la carta.
+     */
     public int getNumBueyes(){
         return numBueyes;
     }
+
+    /**
+     *
+     * @return Devuelve el número de la carta.
+     */
     public int getNumCarta(){
         return numCarta;
     }
+
+    /**
+     *
+     * @return Devuelve la carta en formato: [Número: XXX] [Bueyes: XXX]
+     */
     @Override
     public String toString(){
         StringBuilder sb = new StringBuilder();
